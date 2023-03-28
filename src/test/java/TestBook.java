77579e3033fbee24b472a7ff455bd5caba069277
KIra50Hh <antoine_Arnoud@etu.u-bourgogne.fr>
2023-03-28 14:42:50 +0200

    First commit

---
+++ src/test/java/TestBook.java
@@ -0,0 +1,9 @@
+
+public class TestBook {
+
+    public static void main(String[] args) {
+        Book book = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954);
+        System.out.println(book);
+    }
+}
+
