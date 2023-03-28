017eb7071791826abd0e0957bacc783b8e229317
KIra50Hh <antoine_Arnoud@etu.u-bourgogne.fr>
2023-03-28 15:02:22 +0200

    First commit

---
+++ src/test/java/TestBank.java
@@ -0,0 +1,13 @@
+import bank.Account;
+
+public class TestBank {
+    public static void main(String[] args) {
+        Account account1 = new Account("John", 1000, 0.5);
+        Account account2 = new Account("Jane", 2000, 0.5);
+        account1.transfert(account2, 500);
+        System.out.println(account1);
+        System.out.println(account2);
+    }
+
+
+}
